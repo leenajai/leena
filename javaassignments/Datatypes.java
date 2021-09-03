b99edffb04a5aa542523b9f085586b07fa517688
leenajai <90033423+leenajai@users.noreply.github.com>
2021-09-03 21:19:55 +0530

    First Assignment Commit

---
+++ javaassignments/Datatypes.java
@@ -0,0 +1,15 @@
+public class Datatypes {
+    public static void main(String args[]) {
+      int a=10;
+      float b=25.67867f;
+      double c=893;
+      char d='T';
+      String e="Java" ;
+
+      System.out.println("int = "+a);
+      System.out.println("Float = "+b);
+      System.out.println("Double = "+c);
+      System.out.println("Character = "+d);
+      System.out.println("String = "+e);
+    }
+}
